966154ba0ef5d41e8b18125dac4b19406e53275f
xcibe95x <xcibe95x@gmail.com>
2022-06-01 00:46:53 +0200

    Make dynamic call

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 let amount = 0;
-let btn1 = document.getElementById("b1");
-let btn2 = document.getElementById("b2");
-let btn3 = document.getElementById("b3");
-let btn4 = document.getElementById("b4");
+let jsonData = JSON.parse(products);
+let imgCarousel = document.querySelector(".array-images");
+let mainImage = document.querySelector(".preview");
+
 function updateCounter() {
     document.getElementById("amount").innerHTML = parseInt(amount);
 }
-function updateImage(imgsrc) {
-    document.getElementById("thumb").src = imgsrc.replace("-thumbnail", "");
-}
+
 document.getElementById("dec")
 .addEventListener("click", () => {
    if (amount != 0) --amount;
@@ -18,39 +16,20 @@ document.getElementById("inc")
 .addEventListener("click", () => {
     ++amount; updateCounter();
 });
-document.getElementById("b1")
-.addEventListener("click", () => {
-    updateImage(
-        btn1.firstChild.src);
-        btn1.classList.add("img-active");
-        btn2.classList.remove("img-active")
-        btn3.classList.remove("img-active")
-        btn4.classList.remove("img-active")
-});
-document.getElementById("b2")
-.addEventListener("click", () => {
-    updateImage(
-        btn2.firstChild.src);
-        btn2.classList.add("img-active");
-        btn1.classList.remove("img-active")
-        btn3.classList.remove("img-active")
-        btn4.classList.remove("img-active")
-});
-document.getElementById("b3")
-.addEventListener("click", () => {
-    updateImage(
-    btn3.firstChild.src);
-    btn3.classList.add("img-active");
-    btn2.classList.remove("img-active")
-    btn1.classList.remove("img-active")
-    btn4.classList.remove("img-active")
-});
-document.getElementById("b4")
-.addEventListener("click", () => {
-    updateImage(
-        btn4.firstChild.src);
-        btn4.classList.add("img-active");
-        btn2.classList.remove("img-active")
-        btn3.classList.remove("img-active")
-        btn1.classList.remove("img-active")
+
+for (i = 0; i < jsonData.length; i++) {
+    let button = document.createElement("button");
+    imgCarousel.append(button);
+    let image = document.createElement("img");
+    image.src = jsonData[i];
+    button.append(image);
+    button.classList.add("btn");
+}
+
+const images = document.querySelectorAll(".btn");
+images.forEach((element) => {
+    element.addEventListener("click", () => {
+        mainImage.src = element.firstChild.src.replace("-thumbnail", "");
+        element.classList.add("img-active");
+    })
 });
